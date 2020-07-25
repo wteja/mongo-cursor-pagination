444165127b53a09b32750e2978e876b0d0e43297
Brad Vogel <brad@mixmax.com>
2020-07-25 10:30:50 -0700

    fix(bson): fixes regression where string _ids were no longer supported
    Fixes regression in https://github.com/mixmaxhq/mongo-cursor-pagination/pull/134 whereby in moving
    from mongodb-extended-json to bson for encoding cursors, we accidentally broke support for string
    _ids. This is because EJSON.stringify doesn't (and never did) support encoding strings, as it is not
    a superset of JSON.stringify's functionality as we had assumed. This PR adds support back by in a
    safe way by using JSON.stringify to encode all primitive (ie non-object) values.

--- src/utils/bsonUrlEncoding.js
+++ src/utils/bsonUrlEncoding.js
@@ -7,7 +7,16 @@ const base64url = require('base64-url');
  */
 
 module.exports.encode = function(obj) {
-  return base64url.encode(EJSON.stringify(obj));
+  let jsonEncoded;
+  if (typeof obj !== 'object') {
+    // In the special case that a string is passed in, use JSON instead of ESON, as EJSON
+    // can only encode objects. EJSON.parse, however, can decode strings properly, so there's no
+    // need to special case below.
+    jsonEncoded = JSON.stringify(obj);
+  } else {
+    jsonEncoded = EJSON.stringify(obj);
+  }
+  return base64url.encode(jsonEncoded);
 };
 
 module.exports.decode = function(str) {
